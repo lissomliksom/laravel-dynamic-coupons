d04d4da7d7a3bed08e191c12964e70ef69357e2b
Simen Kristoffersen <lissomliksom@gmail.com>
2025-01-17 08:06:55 +0100

    Minor updates to colors

--- resources/views/components/notifications.blade.php
+++ resources/views/components/notifications.blade.php
@@ -2,18 +2,17 @@
     'message' => 'Hei',
     'color' => 'white',
     'colors' => [
-        'white' => 'bg-white focus:ring-gray-900 active:ring-gray-900',
-        'light' => 'bg-gray-50 focus:ring-gray-900 active:ring-gray-900',
-        'dark' => 'bg-gray-900 focus:ring-emerald-700 active:ring-emerald-700',
-        'darker' => 'bg-gray-1000 focus:ring-emerald-700 active:ring-emerald-700',
+        'blue' => 'bg-blue-50 border-blue-900',
+        'light' => 'bg-zinc-50 border-zinc-900',
     ],
 ])
 
 @if(strlen($message) > 0)
   <article
       {{ $attributes->merge(['class' => "
-          w-full mb-6 border rounded-lg shadow-sm p-6
+          w-full mb-6 border-2 rounded-lg shadow-sm p-6
           {$colors[$color]}
       "]) }}>
+      {{ $message }}
   </article>
 @endif
