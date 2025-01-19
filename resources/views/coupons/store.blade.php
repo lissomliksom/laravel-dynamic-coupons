05bfa73a062665804a29195d8c9bbc95051be807
lissomliksom <lissomliksom@gmail.com>
2025-01-19 12:54:25 +0100

    Implement rudimentary message

--- resources/views/coupons/store.blade.php
+++ resources/views/coupons/store.blade.php
@@ -5,4 +5,7 @@
         <span class="px-2 py-1 text-xs text-white rounded-sm bg-zinc-950">store</span>
         <em class="text-zinc-500">Viser aktuelle rabattkoder for {{ $email }}</em>
     </div>
+    
+    <x-notification :message="$message" />
+
 @endsection
